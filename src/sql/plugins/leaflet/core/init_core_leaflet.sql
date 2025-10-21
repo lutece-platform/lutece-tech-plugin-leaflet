24213d9be9d035ebf4903df6663796da8e315cab
obfromevi <obfromevi@gmail.com>
2025-10-21 15:02:19 +0200

    LUT-30988 : add liquibase tags and remove empty SQL files

--- src/sql/plugins/leaflet/core/init_core_leaflet.sql
+++ src/sql/plugins/leaflet/core/init_core_leaflet.sql
@@ -1,3 +1,6 @@
+-- liquibase formatted sql
+-- changeset leaflet:init_core_leaflet.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 INSERT INTO core_datastore VALUES ('leaflet.icon.icons.default.installed', 'true');
 INSERT INTO core_datastore VALUES ('leaflet.icon.icons.red.installed', 'true');
 INSERT INTO core_datastore VALUES ('leaflet.icon.icons.yellow.installed', 'true');
